b838ed3ff1ce026df771401b66bfd3039e0479fa
Josh Samuelson <js@puppetlabs.com>
2015-10-16 12:54:59 -0700

    Update init.pp

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,48 +1,38 @@
 class nginx {
+# Windows owner/group: Administrator / Administrators
+File {
+owner => 'root',
+group => 'root',
+mode => '0664',
+}
 # Windows package name: nginx-service
 package { 'nginx':
 ensure => present,
 }
 # Windows path: C:/ProgramData/nginx/html
-# Windows owner/group: Administrator / Administrators
 file { '/var/www':
 ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
 }
 # Windows path: C:/ProgramData/nginx/html/index.html
-# Windows owner/group: Administrator / Administrators
 file { '/var/www/index.html':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
 source => 'puppet:///modules/nginx/index.html',
 }
 # Windows path: C:/ProgramData/nginx/conf/nginx.conf
-# Windows owner/group: Administrator / Administrators
 file { '/etc/nginx/nginx.conf':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
 source => 'puppet:///modules/nginx/nginx.conf',
 require => Package['nginx'],
-notify=> Service['nginx'],
+notify => Service['nginx'],
 }
 # Windows path: C:/ProgramData/nginx/conf.d/default.conf
-# Windows owner/group: Administrator / Administrators
 file { '/etc/nginx/conf.d/default.conf':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',source => 'puppet:///modules/nginx/default.conf',
-require => Package['nginx'],
+source => 'puppet:///modules/nginx/default.conf',
 notify => Service['nginx'],
+require => Package['nginx'],
 }
 service { 'nginx':
 ensure => running,
 enable => true,
-}
-}
+}}
