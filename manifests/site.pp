659bde718d3e59c82aa341fa7c9a09a951e53873
Josh Samuelson <js@puppetlabs.com>
2015-10-16 13:12:30 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  notify { "Hello, my name is Josh": }
-  include hosts
-  include skeleton
-  include motd
-  include nginx
-  if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
-notify { "This is a ${vmname} virtual machine.": }
-}
+  include users::admins
 }
